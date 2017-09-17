6af1a1286dc2dc45ffb5395ff6281ffbfef3ab7d
mathew <mathewvermeer@hotmail.com>
2017-09-18 00:40:11 +0200

    add TODO and fix add barplot

--- R/Econ of cybersec test.R
+++ R/Econ of cybersec test.R
@@ -1,12 +1,18 @@
 #libraries needed
-#install.packages("ggplot2")
+# install.packages("ggplot2")
 library("ggplot2")
+library(plyr)
 
 #Download data
 ransomware <- read.csv("https://ransomwaretracker.abuse.ch/feeds/csv/",header = TRUE,sep = ",",skip = 8)
 
 #Clean data:
-#Todo: Clean countries, translate URL to TLD, 
+#Todo: translate URL to TLD, 
+ransomware[, "Country"] = substring(ransomware[, "Country"], 0, 2)
+ransomware = ransomware[!(is.na(ransomware$Country) | ransomware$Country==""), ]
+countryCounts = count(ransomware, "Country")
+topCountries = countryCounts[countryCounts$freq > 200, ]
+sortedCountries = topCountries[order(-topCountries$freq), ]
 
 #Investigate data:
 #Investigate coocurance malware and threat
@@ -25,7 +31,8 @@ barplot(table(ransomware['Malware']), ylab = "Frequency",las = 2)
 
 #Plot frequency of countries
 #Todo: plot only most occuring, color coded mallware
-barplot(sort(table(ransomware['Country'])), ylab = "Frequency",las = 2, horiz = TRUE)
+# barplot(sort(table(ransomware['Country'])), ylab = "Frequency",las = 2, horiz = TRUE)
+barplot(sortedCountries$freq, names=sortedCountries$Country, xlab = "Country", ylab = "Frequency", main = "Infections per country")
 
 #Plot frequency per registration company
 #Todo: Only plot most occuring, indicate different mallware with different color
--- README.md
+++ README.md
@@ -6,3 +6,7 @@
 - Thomas van Biemen / 4206827
 - Samuel Natalius / 4608380
 - Mathew Vermeer / 4216989
+
+### TODO
+
+- Group infections by IP to get true infection frequencies.
